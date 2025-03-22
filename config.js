8f9599c0ebfed14dff99e289aabf6a769f43ee7d
Sarif167 <wajsarif461@gmail.com>
2025-03-23 01:29:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ global.keysneoxrrr = ['5VC9rvNx', 'cfALv5']
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())]
 global.lolkeysapi = ['GataDios']
 
-global.canal = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.canal = 'https://chat.whatsapp.com/BZcQH9xN8ff2WdrjHok8pJ'
 
 
 global.APIs = {
@@ -77,11 +77,11 @@ global.packname = 'MEGA-AI'
 global.author = 'GlobalTechInfo'
 global.menuvid = 'https://i.imgur.com/2U2K9YA.mp4'
 global.igfg = ' Follow on Instagram\nhttps://www.instagram.com/global.techinfo'
-global.dygp = 'https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07'
+global.dygp = 'https://chat.whatsapp.com/BZcQH9xN8ff2WdrjHok8pJ'
 global.fgsc = 'https://github.com/GlobalTechInfo/MEGA-AI'
 global.fgyt = 'https://youtube.com/@GlobalTechInfo'
 global.fgpyp = 'https://youtube.com/@GlobalTechInfo'
-global.fglog = 'https://i.ibb.co/G2dh9cB/qasim.jpg'
+global.fglog = 'https://i.ibb.co/99PQTxqr/8a31ab5494e4.jpg'
 global.thumb = fs.readFileSync('./assets/A.jpg')
 
 global.wait = '⏳'
